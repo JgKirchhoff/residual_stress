4ce20b0730a0e076ec2596bf90c55e877b30b2c4
jgkirchhoff <josephgkirchhoff.com>
2022-11-23 17:59:56 -0600

    Made heat penetration work

--- heat_transfer.py
+++ heat_transfer.py
@@ -22,7 +22,7 @@ x_l = 10 #mm - Heating length
 t_l = x_l/v_p # s - Irradiation times
 t_l = 1.5 # seconds
 w_tape = 6.35 #mm - Tape Width
-p_l = 470 #W - Laser Power
+p_l = 100 #W - Laser Power
 q0 =  p_l/(x_l*w_tape) #W/mm^2 - Heat Flux
 
 alpha = 0.32 #mm^2/s
@@ -33,40 +33,63 @@ Tp = 370 #C
 Td = 550 #C
 T0 = 20 #C <- Set this equal to mold temperature
 
-# Plotting heat penetration depth
-# time = [0.001, 0.0025, 0.005, 0.0075, 0.01, 0.05, 0.1]#np.linspace(0.01,1,20) #seconds
+# Plotting heat penetration depth for irradiation
+# time = np.linspace(0.01,1,20) #seconds
 # T_list = []
 # for t in time:
 #     z = 0
 #     for z in z_steps:
 #         calc = T0 + ((2*q0)/k_z)*math.sqrt(alpha)*math.sqrt(t)*ierfc(z/math.sqrt(4*alpha*t)) #heat penetration
 #         velocity = x_l/t
-#         T_list.append([z,str(velocity/1000),calc])
+#         T_list.append([z,t,calc])
 
-# df = pd.DataFrame(T_list,columns=['z (mm)','velocity (m/seconds)','temp (C)']) #heat penetration
-# fig = px.scatter(df, x="z (mm)",y="temp (C)",color="velocity (m/seconds)",title="heat penetration per velocity",color_continuous_scale='Inferno') #heat penetration
+# df = pd.DataFrame(T_list,columns=['z (mm)','irradiation time (seconds)','temp (C)']) #heat penetration
+# fig = px.scatter(df, x="z (mm)",y="temp (C)",color="irradiation time (seconds)",title="heat penetration per irradiation time",color_continuous_scale='Inferno') #heat penetration
+# fig.update_layout(legend=dict(title_font_family="Times New Roman",
+#                               font=dict(size= 10)))
+# #if not os.path.exists("/images"):
+#  #   os.mkdir("/images")
 
-# if not os.path.exists("res_stress/images"):
-#     os.mkdir("res_stress/images")
+# #fig.show()
+# fig.write_image("res_stress/images/heatpenetration_vs_irradiation.eps")
 
+
+# # Plotting velocity with heat penetration
+# time = np.linspace(0.01,1,20) #seconds
+# T_list = []
+# for t in time:
+#     z = 0
+#     for z in z_steps:
+#         calc = T0 + ((2*q0)/k_z)*math.sqrt(alpha)*math.sqrt(t)*ierfc(z/math.sqrt(4*alpha*t)) #heat penetration
+#         velocity = x_l/t
+#         T_list.append([z,str(round(velocity,4)),calc])
+
+# df = pd.DataFrame(T_list,columns=['z (mm)','velocity (mm/seconds)','temp (C)']) #heat penetration
+# fig = px.scatter(df, x="z (mm)",y="temp (C)",color="velocity (mm/seconds)",title="heat penetration per velocity",color_continuous_scale='Inferno') #heat penetration
+# fig.update_layout(legend=dict(title_font_family="Times New Roman",
+#                               font=dict(size= 10)))
+# #if not os.path.exists("/images"):
+#  #   os.mkdir("/images")
+
+# fig.show()
 # fig.write_image("res_stress/images/heatpenetration_vs_velocity.eps")
 
-
-# Plotting cooling over time
-time = np.linspace(0.01,1,25) #seconds
+# Heat Penetration, Constant Velocity, Changing Power
+time = np.linspace(0.01,1,20) #seconds
 T_list = []
 for t in time:
     z = 0
     for z in z_steps:
         calc = T0 + ((2*q0)/k_z)*math.sqrt(alpha)*math.sqrt(t)*ierfc(z/math.sqrt(4*alpha*t)) #heat penetration
         velocity = x_l/t
-        T_list.append([z,str(velocity/1000),calc])
+        T_list.append([z,t,calc])
 
-df = pd.DataFrame(T_list,columns=['z (mm)','velocity (m/seconds)','temp (C)']) #heat penetration
-fig = px.scatter(df, x="z (mm)",y="temp (C)",color="velocity (m/seconds)",title="heat penetration per velocity",color_continuous_scale='Inferno') #heat penetration
+df = pd.DataFrame(T_list,columns=['z (mm)','irradiation time (seconds)','temp (C)']) #heat penetration
+fig = px.scatter(df, x="z (mm)",y="temp (C)",color="irradiation time (seconds)",title="heat penetration per irradiation time",color_continuous_scale='Inferno') #heat penetration
+fig.update_layout(legend=dict(title_font_family="Times New Roman",
+                              font=dict(size= 10)))
+#if not os.path.exists("/images"):
+ #   os.mkdir("/images")
 
-if not os.path.exists("res_stress/images"):
-    os.mkdir("res_stress/images")
-
-fig.show()
-#fig.write_image("res_stress/images/heatpenetration_vs_velocity.eps")
+#fig.show()
+fig.write_image("res_stress/images/heatpenetration_vs_irradiation.eps")
